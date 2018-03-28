66b2b2c2f6bbb60d133169fbbd114da9a8e608e7
Choubakawa <Choubakawa@users.noreply.github.com>
2018-03-28 18:28:05 +0200

    add config value

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ if (typeof process != 'undefined') {
 }
 
 module.exports = {
+  "lang": "fr-fr",
   "token": envVars.TOKEN,
   "name": "utopiaBot",
   "password": envVars.PASSWORD,
@@ -20,5 +21,6 @@ module.exports = {
       }
     ]
   },
+  "messageTitle" : "Token Discord",
   "message" : "Hello {pseudo}, \n \n Merci pour ta demande, on est content que tu veuilles rejoindre la nouvelle taverne du forum ! \n Voici le message à copier/ coller sur Discord pour être validé : \n \n [code]!UB token {token}[/code] \n \n Ton pseudo sera automatiquement changé si besoin et tu seras ajouté au groupe des membres vérifiés ;) \n \n -- \n Si tu n'as pas fait de demande pour être ajouté au Discord (nouvelle chatbox) de Never Utopia, nous sommes désolés :() \n N'hésite pas à nous le signaler ou envoyer un MP à Choubakawa pour nous prévenir que quelqu'un se fait passer pour toi."
   }
