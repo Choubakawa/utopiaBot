db51bbdd0f72a4877e3a880a403628c72de86647
Choubakawa <Choubakawa@users.noreply.github.com>
2018-03-28 18:14:38 +0200

    change name user

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (typeof process != 'undefined') {
 
 module.exports = {
   "token": envVars.TOKEN,
-  "name": "Choubakawa",
+  "name": "utopiaBot",
   "password": envVars.PASSWORD,
   "rolesID":{
     "verify": envVars.VERIFY
@@ -20,5 +20,5 @@ module.exports = {
       }
     ]
   },
-  "message" : "Hello {pseudo}, \n \n Merci pour ta demande, on est content que tu veuilles rejoindre la nouvelle taverne du forum ! \n Voici le message à copier/ coller sur Discord pour être validé : \n \n [code]!UB token ylet2qmloiyxdrrqky1zto6r[/code] \n \n Ton pseudo sera automatiquement changé si besoin et tu seras ajouté au groupe des membres vérifiés ;) \n \n -- \n Si tu n'as pas fait de demande pour être ajouté au Discord (nouvelle chatbox) de Never Utopia, nous sommes désolés :() \n N'hésite pas à nous le signaler ou envoyer un MP à Choubakawa pour nous prévenir que quelqu'un se fait passer pour toi."
-}
+  "message" : "Hello {pseudo}, \n \n Merci pour ta demande, on est content que tu veuilles rejoindre la nouvelle taverne du forum ! \n Voici le message à copier/ coller sur Discord pour être validé : \n \n [code]!UB token {token}[/code] \n \n Ton pseudo sera automatiquement changé si besoin et tu seras ajouté au groupe des membres vérifiés ;) \n \n -- \n Si tu n'as pas fait de demande pour être ajouté au Discord (nouvelle chatbox) de Never Utopia, nous sommes désolés :() \n N'hésite pas à nous le signaler ou envoyer un MP à Choubakawa pour nous prévenir que quelqu'un se fait passer pour toi."
+  }
