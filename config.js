32cca9e647d7b2ea3ce9fcb1fa5e4366dcfd72b1
Choubakawa <Choubakawa@users.noreply.github.com>
2018-04-08 20:12:38 +0200

    ajoute les roles correspondants aux groupes de l'utilisateur

--- config.js
+++ config.js
@@ -13,36 +13,34 @@ module.exports = {
   "rolesID":{
     "verify": envVars.VERIFY
   },
-  "forum":{
+ "forum":{
     "link": "http://planellum-fornax.forumactif.org",
     "groups" : [
       {
         "name" : "Membres de Planellum",
         "link" : "/g4-membres-de-planellum",
-        "discordRoleId" : ""
+        "discordRoleId" : envVars.MEMBERS
       },
       {
         "name" : "Amis de Planellum",
         "link" : "/g9-amis-de-planellum",
-        "discordRoleId" : ""
+        "discordRoleId" : envVars.FRIENDS
       },
       {
         "name" : "Candidats",
         "link" : "/g19-candidats",
-        "discordRoleId" : ""
+        "discordRoleId" : envVars.APPLICANTS
       },
       {
         "name" : "Administrateurs",
         "link" : "/g1-administrateurs",
-        "discordRoleId" : ""
+        "discordRoleId" : envVars.ADMINS
       },
       {
         "name" : "Padawan",
         "link" : "/g12-padawan",
-        "discordRoleId" : ""
+        "discordRoleId" : envVars.PADAWAN
       }
     ]
-  },
-  "messageTitle" : "Token Discord",
-  "message" : "Hello {pseudo}, \n \n Merci pour ta demande, on est content que tu veuilles rejoindre la nouvelle taverne du forum ! \n Voici le message à copier/ coller sur Discord pour être validé : \n \n [code]!UB token {token}[/code] \n \n Ton pseudo sera automatiquement changé si besoin et tu seras ajouté au groupe des membres vérifiés ;) \n \n -- \n Si tu n'as pas fait de demande pour être ajouté au Discord (nouvelle chatbox) de Never Utopia, nous sommes désolés :() \n N'hésite pas à nous le signaler ou envoyer un MP à Choubakawa pour nous prévenir que quelqu'un se fait passer pour toi."
   }
+ }
